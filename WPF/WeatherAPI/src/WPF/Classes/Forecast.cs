41aa1ddfb2d28da395f6d019ce47229496727166
Mishin Yura <mishin.inbox@gmail.com>
2021-04-08 10:13:04 +0300

    Displayed the Results using a GridView. Closes #20

---
+++ WPF/WeatherAPI/src/WPF/Classes/Forecast.cs
@@ -0,0 +1,9 @@
+namespace WPF.Classes
+{
+  public class Forecast
+  {
+    public string Day { get; set; }
+    public string Temperature { get; set; }
+    public string Wind { get; set; }
+  }
+}
--- WPF/WeatherAPI/src/WPF/MainWindow.xaml.cs
+++ WPF/WeatherAPI/src/WPF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 using System.Net.Http;
 using System.Windows;
+using Newtonsoft.Json;
+using WPF.Classes;
 
 namespace WPF
 {
@@ -19,6 +21,8 @@ namespace WPF
       using var client = new HttpClient();
       var response = await client.GetAsync(url);
       var responseString = await response.Content.ReadAsStringAsync();
+      var forecast = JsonConvert.DeserializeObject<WeatherExample>(responseString).Forecast;
+      ForecastListView.ItemsSource = forecast;
     }
   }
 }
